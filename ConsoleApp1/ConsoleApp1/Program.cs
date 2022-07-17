57193c95a26e7b06f75ac233a7ceb2bd8547a519
johndward01 <johndward01@gmail.com>
2022-07-17 05:09:15 -0500

    update

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+//Console.Write("Please enter your name: ");
+//var name = Console.ReadLine();
+//Console.WriteLine($"Hello, {name}!");
+
+
+var util = new Utility();
+var cubed = util.cube.Invoke(3);
+Console.WriteLine(cubed);
